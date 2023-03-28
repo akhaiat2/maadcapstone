c72de14009d08128ee6abd2be42b5d5df0c1747f
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2023-03-28 14:54:16 -0500

    Update Stock.js

--- stockproject/src/Stock.js
+++ stockproject/src/Stock.js
@@ -63,7 +63,7 @@ class Stock extends React.Component {
     }
 
 	setup = (p5, parentRef) => {
-		p5.createCanvas(1200, 380).parent(parentRef);
+		p5.createCanvas(1200, 750).parent(parentRef);
         p5.frameRate(7)
 	};
 
