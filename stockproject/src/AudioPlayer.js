2544938cbf9df90864de3a9365e4a75d7d888a87
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2023-04-03 13:57:22 -0400

    still fixing audio

--- stockproject/src/AudioPlayer.js
+++ stockproject/src/AudioPlayer.js
@@ -3,9 +3,12 @@ import React from "react";
 class AudioPlayer extends React.Component {
   render() {
     return (
-      <audio autoPlay loop>
-        <source src='stockproject/src/assets/background.mp3' type='audio/mpeg' />
-      </audio>
+      <div>
+        <audio autoPlay loop>
+            <source src='stockproject/src/assets/background.mp3' type='audio/mpeg' />
+        </audio>
+        {console.log('audio playing')}
+      </div>
     )
   }
 }
