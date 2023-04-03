b672f3745d8e1bbf97d34b786ad9ef6cf0ef9cd0
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2023-04-03 14:10:20 -0400

    trying different method for background audio

--- stockproject/src/App.js
+++ stockproject/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import Stock from './Stock'
 import './App.css'
-import ReactDOM from 'react-dom';
-import AudioPlayer from './AudioPlayer'
+import ReactDOM from 'react-dom'
+import audioFile from './background.mp3'
 
 class App extends React.Component {
-    render () {
+  componentDidMount () {
+    this.audioPlayer.play()
+  }
+  render () {
 		return (
             <div className="App">
                 <Stock></Stock>
-                <AudioPlayer />
+                <audio
+                    ref={(element) => { this.audioPlayer = element; }}
+                    autoPlay
+                >
+                <source src={audioFile} type="audio/mpeg" />
+                </audio>
             </div>
 		)
 	}
